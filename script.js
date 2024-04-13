00c1f9eb41c3efd466e9ec0633d00a7cab1dd52b
LQuesadaM <luisquesada163@gmail.com>
2024-04-13 20:33:54 -0300

    🍻 Added methodo that add item to cart

--- script.js
+++ script.js
@@ -103,4 +103,11 @@ class ShoppingCart {
     this.total = 0;
     this.taxRate = 8.25;
   }
+
+  addItem(id, products) {
+    let product = products.find((item) => item.id === id);
+    const { name, price } = product;
+    this.items.push(product);
+    
+  }
 }
