d4bdc0ee590eb2f009bde16e358b9bb9507f42ba
LQuesadaM <luisquesada163@gmail.com>
2024-04-13 19:51:07 -0300

    🍻 set in the screen the list

--- script.js
+++ script.js
@@ -85,3 +85,14 @@ const products = [
     category: "Cupcake",
   },
 ];
+
+products.forEach(({ name, id, price, category }) => {
+  dessertCards.innerHTML += `
+    <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button class="btn add-to-cart-btn" id="${id}">Add to cart</button>
+    </div>
+    `;
+});
